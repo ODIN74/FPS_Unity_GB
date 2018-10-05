9eb7e487bd9ba66f7c2826217b8abc55c0ddad04
ODIN74 <fantompus@gmail.com>
2018-10-05 09:43:21 +0500

    Добавлен плагин генерации путевых точек для объекта

---
+++ Assets/Editor/MenuItems.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace WayPointsGenerator
+{
+    public class MenuItems
+    {
+        [MenuItem("Window/Experimental/WayPointsGenerator &#p",false,100000)]
+        public static void OpenWayPointsGenerator()
+        {
+            WayPointsGeneratorWindow.ShowWindow();
+        }
+    }
+}
+
+
